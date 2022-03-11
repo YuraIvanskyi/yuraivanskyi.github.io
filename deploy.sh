2c8f206ba82c1e054745bea4473185faacd49148
Yuriy Ivanskyi <yuraivanskyi@gmail.com>
2022-03-11 14:25:02 +0200

    Readme, config, deploy

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,6 @@ npm run build
 cd dist
 
 git init
-git checkout -b main
 git add -A
 git commit -m 'deploy'
 
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    minify: true,
+  },
+  publicDir: 'dist',
   server: {
     host: true,
   },
