bdc100411397e6e64745efb4068b1fa6e3ee7456
Yuriy Ivanskyi <yuraivanskyi@gmail.com>
2022-03-11 12:48:05 +0200

    deploy script

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+git init
+git checkout -b main
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:yuraivanskyi/yuraivanskyi.github.io.git main
+
+cd -
