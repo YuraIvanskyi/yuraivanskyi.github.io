5fb50e649a99fb4eb7f1b5262010d7e1c4d48088
Yura Ivanskyi <yuraivanskyi@gmail.com>
2022-08-25 23:03:49 +0300

    enable taiwind, toy with grids

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,52 @@
 <script setup>
-
+import Card from './components/sub-components/card.vue';
 </script>
 
 <template>
-  <div></div>
+  <div class="h-screen w-auto p-4 
+            pantone-yellow">
+    <div class="grid h-full grid-rows-4 grid-cols-4 gap-4">
+      <div class="">
+        <card title="1" />
+      </div>
+      <div class="">
+        <card :title="2" />
+      </div>
+      <div class="">
+        <card :title="3" />
+      </div>
+      <div class="row-start-1 row-end-3 row-span-2">
+        <card :title="4" />
+      </div>
+      <div class="col-span-2">
+        <card :title="5" />
+      </div>
+      <div class="">
+        <card :title="6" />
+      </div>
+      <div class="">
+        <card :title="7" />
+      </div>
+      <div class="">
+        <card :title="8" />
+      </div>
+      <div class="">
+        <card :title="9" />
+      </div>
+      <div class="">
+        <card :title="10" />
+      </div>
+      <div class="">
+        <card :title="11" />
+      </div>
+      <div class="">
+        <card :title="12" />
+      </div>
+      <div class="col-span-2">
+        <card :title="13" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <style>
@@ -13,6 +56,21 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.pantone-yellow {
+  background-color: rgb(255, 213, 0);
+}
+
+.pantone-yellow-text {
+  color: rgb(255, 213, 0);
+}
+
+.pantone-blue {
+  background-color: rgb(0, 91, 187);
+}
+
+.pantone-vlue-text {
+  color: rgb(0, 91, 187);
 }
 </style>
---
+++ src/components/sub-components/card.vue
@@ -0,0 +1,26 @@
+<script setup>
+defineProps({
+    title: String,
+    subtitle: String,
+    socialCard: Boolean
+})
+</script>
+
+<template>
+    <div class="pantone-blue
+                shadow-2xl rounded-md hover:rounded-3xl transition duration-300 my-auto h-full">
+        <div class="px-4 py-4 text-center sm:text-left sm:px-6">
+            <h3 class="text-lg leading-6 font-medium pantone-yellow-text">{{ title }}</h3>
+            <p class="text-sm text-gray-300">{{ subtitle }}</p>
+        </div>
+        <!-- <div class="border-t border-gray-200 px-2 py-2"> -->
+        <slot></slot>
+        <!-- </div> -->
+    </div>
+</template>
+
+<style>
+.border-1 {
+    border-width: 1px;
+}
+</style>
