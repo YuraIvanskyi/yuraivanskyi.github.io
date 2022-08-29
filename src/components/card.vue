4c1eccf1f58d07e9e474a15bf522b6100786698a
Yura Ivanskyi <yuraivanskyi@gmail.com>
2022-08-29 23:44:47 +0300

    Content optimisation, layouts 90% done

--- src/components/card.vue
+++ src/components/card.vue
@@ -6,17 +6,18 @@ defineProps({
 </script>
 
 <template>
-    <div class="pantone-blue shadow-2xl rounded-2xl h-auto m-auto
-                p-2 md:p-3 lg:p-4 xl:p-6
+    <div class="pantone-blue shadow-2xl rounded-2xl h-auto w-auto mx-auto
+                p-4 md:p-5 lg:p-6 xl:p-8
+                align-middle flex flex-col
               hover:shadow-blue-500 transition duration-300 ">
         <div class="mb-1 md:mb-2 lg:mb-4 xl:mb-6">
             <div class="flex justify-between">
-                <p class="text-3xl text-center sm:text-left leading-8 font-bold pantone-yellow-text">{{ title }}</p>
+                <p class="text-3xl text-left leading-8 font-bold pantone-yellow-text">{{ title }}</p>
                 <div class="text-right pantone-yellow-text">
                     <slot name="icon"></slot>
                 </div>
             </div>
-            <p class="text-lg text-center sm:text-left font-semibold text-gray-300">{{ subtitle }}</p>
+            <p class="text-lg text-left font-semibold text-gray-300">{{ subtitle }}</p>
         </div>
 
         <div class="flex flex-wrap flex-row gap-2">
