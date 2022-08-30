bcb9629135a61927300dcdfab16542e06d104e34
Yura Ivanskyi <yuraivanskyi@gmail.com>
2022-08-30 23:16:32 +0300

    tweaked stuff

--- src/components/card.vue
+++ src/components/card.vue
@@ -10,17 +10,17 @@ defineProps({
                 p-4 md:p-5 lg:p-6 xl:p-8
                 align-middle flex flex-col
               hover:shadow-blue-500 transition duration-300 ">
-        <div class="mb-1 md:mb-2 lg:mb-4 xl:mb-6">
+        <div class="mb-1 md:mb-2 lg:mb-4 xl:mb-6 flex-shrink">
             <div class="flex justify-between">
-                <p class="text-3xl text-left leading-8 font-bold pantone-yellow-text">{{ title }}</p>
+                <p class="text-3xl text-left leading-8 font-bold pantone-yellow-text">{{  title  }}</p>
                 <div class="text-right pantone-yellow-text">
                     <slot name="icon"></slot>
                 </div>
             </div>
-            <p class="text-lg text-left font-semibold text-gray-300">{{ subtitle }}</p>
+            <p class="text-lg text-left font-semibold text-gray-300">{{  subtitle  }}</p>
         </div>
 
-        <div class="flex flex-wrap flex-row gap-2">
+        <div class="flex flex-wrap flex-row gap-2 flex-grow">
             <slot name="content"></slot>
         </div>
     </div>
--- src/components/sub-components/tile.vue
+++ src/components/sub-components/tile.vue
@@ -1,4 +1,5 @@
 <script setup>
+
 defineProps({
     type: String,
     size: Number,
@@ -6,7 +7,11 @@ defineProps({
 </script>
 
 <template>
-    <div class="pantone-yellow m-auto h-14 w-14 flex items-center justify-center
+    <div v-if="size !== undefined" :class="`pantone-yellow m-auto h-${size} w-${size} flex items-center justify-center
+    shadow-xl rounded-md hover:scale-150 transition duration-100 cursor-pointer`">
+        <slot></slot>
+    </div>
+    <div v-else class="pantone-yellow m-auto h-14 w-14 flex items-center justify-center
     shadow-xl rounded-md hover:scale-125 transition duration-100 cursor-pointer">
         <slot></slot>
     </div>
