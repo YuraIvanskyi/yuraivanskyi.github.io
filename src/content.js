fbd255be1c0030b2393a70af6af139468191a6aa
Yura Ivanskyi <yuraivanskyi@gmail.com>
2023-06-13 22:56:33 +0300

    Starting preps

---
+++ src/content.js
@@ -0,0 +1,152 @@
+export const randomFacts = [
+    'Experienced gamer: MMORPGs, MOBAs, RPGs, FPS, mobile arcades. In the end, why not?',
+    'I love spicy food and sour drinks! Don\'t mind barbeque with hot sauces up to 2M SHU, thai food along with some lemon fresh)',
+    'Youtube gourmet: watch actors & movie-making things, astrophysics, science, mythology & history, memes',
+    'Listen-to-maniac, literally spend hours listening to various metal genres (power, sympho, core, heavy, folk, japanese etc.), rock, composers like Antti Martikainen, Christian Friedrich Johannes Büttner, Trevor Moris.',
+    'Love sports: football, volleyball, tennis, snooker. Can also easily dare you in table-football!',
+    'Fan of MCU movies & series, fantasy books, great fan of manga and anime (but not a weeb). And "The Boys" - that one is dope as well.'
+];
+
+export const primaryContacts = [
+    'email: yuraivanskyi@gmail.com',
+    'skype: y.ivanskyi'
+]
+
+export const social = [
+    { link: 'https://twitter.com/i1skyi', icon: 'twitter' },
+    { link: 'https://www.facebook.com/yurii.ivanskyi', icon: 'facebook' },
+    { link: 'https://www.instagram.com/yuriyivanskyi/', icon: 'instagram' },
+    { link: 'https://www.linkedin.com/in/yuraivanskyi/', icon: 'linkedin' },
+    { link: 'https://github.com/YuraIvanskyi', icon: 'github' },
+    { link: 'https://www.twitch.tv/i1sky', icon: 'twitch' },
+]
+
+export const techIcons = [
+    { code: 'devicon-python-plain-wordmark', tooltip: 'Python, 3y. exp.' },
+    { code: 'devicon-javascript-plain', tooltip: 'JavaScript, 2y. exp.' },
+    { code: 'devicon-pytest-plain-wordmark', tooltip: 'Pytest' },
+    { code: 'devicon-selenium-original', tooltip: 'Selenium' },
+    { code: 'devicon-html5-plain-wordmark', tooltip: 'HTML5' },
+    { code: 'devicon-css3-plain-wordmark', tooltip: 'CSS3' },
+    { code: 'devicon-vuejs-plain-wordmark', tooltip: 'Vue.js 2/3' },
+    { code: 'devicon-vuetify-line', tooltip: 'Vuetify' },
+    { code: 'devicon-tailwindcss-plain', tooltip: 'Tailwind CSS' },
+    { code: 'devicon-opencv-plain-wordmark', tooltip: 'OpenCV (python)' },
+    { code: 'devicon-firebase-plain-wordmark', tooltip: 'Firebase' },
+    { code: 'devicon-amazonwebservices-original', tooltip: 'AWS Features' },
+    { code: 'devicon-microsoftsqlserver-plain', tooltip: 'MSSQL Server' },
+    { code: 'devicon-docker-plain-wordmark', tooltip: 'Docker' },
+    { code: 'devicon-kubernetes-plain-wordmark', tooltip: 'Kubernetes' },
+    { code: 'devicon-git-plain-wordmark', tooltip: 'Git' },
+    { code: 'devicon-gitlab-plain-wordmark', tooltip: 'Gitlab' },
+    { code: 'devicon-jenkins-line', tooltip: 'Jenkins' },
+    { code: 'devicon-jira-plain', tooltip: 'Jira' },
+    { code: 'devicon-confluence-original-wordmark', tooltip: 'Confluence' },
+]
+
+export const primaryTech = [
+    'game quality assuarance: desktop, web, mobile',
+    'E2E, functional, performance etc. testing on target product',
+    'automation testing using Python, less JavaScript, ',
+    'visual automation testing with opencv',
+    'documentation, test plans, test strategies, reporting',
+]
+
+export const secondaryTech = [
+    'a bit of front-end development (mostly on Vue, Vite, Vuetify)',
+    'handle, organize and lead small team',
+    'nonchalantly talk on meetings',
+]
+
+export const languages = [
+    { lang: 'Ukrainian', level: 100, note: 'Native' },
+    { lang: 'English', level: 90, note: 'Fluent' },
+    { lang: 'Russian', level: 70, note: 'Proffessional' },
+    { lang: 'Japanese', level: 15, note: 'N5 (Beginner)' },
+    { lang: 'German', level: 10, note: 'Beginner' },
+    { lang: 'Polish', level: 10, note: 'Beginner' },
+    { lang: 'Korean', level: 5, note: 'Elementary' },
+]
+
+export const photos = [
+    'src\\assets\\me2.jpg',
+    'src\\assets\\m4.jpg',
+    'src\\assets\\me1.png',
+    'src\\assets\\m3.jpg',
+]
+
+export const jobs = [
+    {
+        title: 'Senior Automation QA - VisiQuate',
+        trivia: 'Career #5: Revamp',
+        start: '08/2022',
+        end: 'Now',
+        desc: 'My role is being a part of an AQA team that strives to optimise QA procedures.',
+        responsibilities: ['contributing to the general test codebase', 'upgrading and supporting automation framework & infrastructure'],
+        current: true
+    },
+    {
+        title: 'Volunteer - Ukraine/IT army of Ukraine',
+        trivia: 'Career #4.5: Fair job',
+        start: '02/2022',
+        end: 'Victory',
+        desc: 'Standing in IT frontline of war. Helped with such tiny yet needed things like using ddos methods to take down Russian civil & military web resourses, showing & sharing the truth about crimes of Russia to the world.',
+        responsibilities: [],
+        current: true
+    },
+    {
+        title: 'Server Test Engineer - Amazon Games',
+        trivia: 'Career #4: Level-up',
+        start: '03/2022',
+        end: '04/2022',
+        desc: 'Cancelled due to russian invasion and inability to relocate to Germany.',
+        responsibilities: ['leading any server test related tasks in Europe',
+            'investigating bugs on live, scale test problems, or other server/service bugs that need more info/logs/metrics'],
+        current: false
+    },
+    {
+        title: 'QA R&D Lead - Gameloft',
+        trivia: 'Career #3: Apex',
+        start: '03/2020',
+        end: '02/2022',
+        desc: 'My main activity was to test the game, find and describe the bugs on assigned tasks.',
+        responsibilities: ['identify and isolate bugs on assigned tasks',
+            'describe found bugs clearly and concisely',
+            'organize and prioritize assigned tasks', 'provide clear and correct task results',
+            'produce valid test cases based on documentation',
+            'create own test cases based on gaming experience and creative point of view', 'coaching and mentoring junior members'],
+        current: false
+    },
+    {
+        title: 'QA Automation - Gameloft',
+        trivia: 'Career #2: Upshift',
+        start: '01/2018',
+        end: '03/2020',
+        desc: 'My role included being both team and tech lead in QA R&D team, creating and delivering solutions for QA process optimisation/automation by means of scripting and/or development.',
+        responsibilities: ['creating E2E scripts for testing purposes',
+            'using internal tools & services to perform automation testing for GUI and server-side cases',
+            'contributing to traditional automation approaches within company',
+            'participating in various game automation initiatives'],
+        current: false
+    },
+    {
+        title: 'QA Tester - Gameloft',
+        trivia: 'Career #1: Init',
+        start: '03/2017',
+        end: '01/2018',
+        desc: 'My main activity was to test the game, find and describe the bugs on assigned tasks.',
+        responsibilities: ['identify and isolate bugs on assigned tasks',
+            'describe found bugs clearly and concisely',
+            'organize and prioritize assigned tasks', 'provide clear and correct task results',
+            'produce valid test cases based on documentation',
+            'create own test cases based on gaming experience and creative point of view', 'coaching and mentoring junior members'],
+        current: false
+    },
+
+]
+
+export const education = [
+    { title: 'Master\'s Degree - Software Engineering', time: '2018-2020', subtitle: 'Lviv Polytechnic National University' },
+    { title: 'Bachelor\'s Degree - Software Engineering', time: '2014-2018', subtitle: 'Lviv Polytechnic National University' },
+    { title: 'Student', time: '2003-2014', subtitle: 'Horodok Gymnasium, Horodok' },
+]
